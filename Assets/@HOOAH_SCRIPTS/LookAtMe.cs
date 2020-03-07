d952da40e8e998548c2752f6676054bc7b0db054
hooh_hooah <hooh_hooah@protonmail.com>
2020-03-08 08:21:54 +0900

    Added AI_Hooah Plugin Components

---
+++ Assets/@HOOAH_SCRIPTS/LookAtMe.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtMe : MonoBehaviour {
+    // I'm nothing
+	void Start () {
+	}
+	void Update () {
+		
+	}
+}
